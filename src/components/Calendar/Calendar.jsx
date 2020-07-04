b783966d676a3d0d6c8ba3fbccaf1b242b14739b
jaybenaim <benaimjacob@gmail.com>
2020-07-04 16:02:42 -0400

    color coated periods

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -3,20 +3,51 @@ import moment from "moment";
 
 import { Calendar as BigCalender, momentLocalizer } from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
-
+import "../../assets/stylesheets/calendar.css";
 const localizer = momentLocalizer(moment);
 
 const Calendar = () => {
-  let period1Start = new Date(2020, 4, 10);
-  let period1End = new Date(2020, 5, 7);
   const myEventsList = [
     {
-      start: period1Start,
-      end: period1End,
+      start: new Date(2020, 2, 15),
+      end: new Date(2020, 3, 10),
       title: "Period 1",
       allDay: false,
     },
+    {
+      start: new Date(2020, 3, 12),
+      end: new Date(2020, 4, 10),
+      title: "Period 2",
+      allDay: false,
+    },
+    {
+      start: new Date(2020, 4, 10),
+      end: new Date(2020, 5, 7),
+      title: "Period 3",
+      allDay: false,
+    },
+    {
+      start: new Date(2020, 5, 7),
+      end: new Date(2020, 6, 5),
+      title: "Period 4",
+      allDay: false,
+    },
+    {
+      start: new Date(2020, 6, 5),
+      end: new Date(2020, 7, 2),
+      title: "Period 5",
+      allDay: false,
+    },
+    {
+      start: new Date(2020, 7, 2),
+      end: new Date(2020, 7, 30),
+      title: "Period 6",
+      allDay: false,
+    },
   ];
+  const onSelectEvent = (e) => {
+    console.log(e);
+  };
   return (
     <div className="container" style={{ height: "100%", width: "100%" }}>
       <BigCalender
@@ -25,6 +56,7 @@ const Calendar = () => {
         startAccessor="start"
         endAccessor="end"
         style={{ height: 500 }}
+        onSelectEvent={onSelectEvent}
       />
     </div>
   );
