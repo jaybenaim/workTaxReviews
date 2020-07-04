5f09ca9a4273cf78088e008367df605a119c2d9d
jaybenaim <benaimjacob@gmail.com>
2020-07-04 17:09:47 -0400

    checkbox toggles 14 vs 28 days for first time clients

--- src/components/FindDate/FindDate.jsx
+++ src/components/FindDate/FindDate.jsx
@@ -1,16 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import moment from "moment";
 
 const FindDate = () => {
   const [date, setDate] = useState(new Date());
-  const [firstTimeApplyingPeriod, set14DaysBack] = useState();
+  const [firstTimeApplyingPeriod, setDaysBack] = useState();
+  const [firstTimeApplying, toggleFirstTimeApplying] = useState(false);
+  const [checkboxValue, setCheckboxValue] = useState(false);
 
-  const handleChange = (dateValue) => {
+  const handleChange = (dateValue, firstTime) => {
     setDate(dateValue);
 
     let dateV = new Date(dateValue);
-    let firstTimeApplyPeriod = dateV.setDate(dateV.getDate() - 12);
-    set14DaysBack(firstTimeApplyPeriod);
+    let amountOfDays = firstTime ? 14 : 28;
+    let firstTimeApplyPeriod = dateV.setDate(
+      dateV.getDate() - (amountOfDays - 2)
+    );
+    setDaysBack(new Date(firstTimeApplyPeriod));
+  };
+
+  const handleCheckbox = (value) => {
+    setCheckboxValue(!checkboxValue);
+    toggleFirstTimeApplying(!firstTimeApplying);
+    handleChange(date, !firstTimeApplying);
   };
 
   return (
@@ -18,6 +29,13 @@ const FindDate = () => {
       <div>Enter the date the client last worked</div>
       {/* Checkbox for 1st time that will change 14 to 28 days */}
       <input type="date" onChange={(e) => handleChange(e.target.value)} />
+      <br />
+      <label htmlFor="checkbox">First Time applying</label>{" "}
+      <input
+        type="checkbox"
+        value={checkboxValue}
+        onChange={(e) => handleCheckbox(e.target.value)}
+      />
       {firstTimeApplyingPeriod && (
         <div>
           Ask The customer if they have made less than $1000 since{" "}
