124b986ba24421d0f3af4b469456503872a02020
jaybenaim <benaimjacob@gmail.com>
2020-07-06 12:03:58 -0400

    added clock panel for timezones

---
+++ build/precache-manifest.d60fc91af3d1782479210cd19e26531d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f6777d5f3c3038c8f5e002115040951",
+    "url": "/workTaxReviews/index.html"
+  },
+  {
+    "revision": "c1f140be596c9195f24d",
+    "url": "/workTaxReviews/static/css/2.c7ed56f2.chunk.css"
+  },
+  {
+    "revision": "7ea0f1a63d9b13f67ca4",
+    "url": "/workTaxReviews/static/css/main.c249e189.chunk.css"
+  },
+  {
+    "revision": "c1f140be596c9195f24d",
+    "url": "/workTaxReviews/static/js/2.4391737e.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/workTaxReviews/static/js/2.4391737e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ea0f1a63d9b13f67ca4",
+    "url": "/workTaxReviews/static/js/main.308a731b.chunk.js"
+  },
+  {
+    "revision": "a9b5967e48fd0111d783",
+    "url": "/workTaxReviews/static/js/runtime-main.d83f61cb.js"
+  }
+]);
