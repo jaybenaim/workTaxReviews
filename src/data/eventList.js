11a68914f21c33083e9e07b1482f78a93b5dc95f
jaybenaim <benaimjacob@gmail.com>
2020-07-06 09:43:53 -0400

    fixed periods

--- src/data/eventList.js
+++ src/data/eventList.js
@@ -1,38 +1,44 @@
 export const myEventsList = [
   {
     start: new Date(2020, 2, 15),
-    end: new Date(2020, 3, 10),
+    end: new Date(2020, 3, 11),
     title: "Period 1",
     allDay: false,
   },
   {
     start: new Date(2020, 3, 12),
-    end: new Date(2020, 4, 10),
+    end: new Date(2020, 4, 9),
     title: "Period 2",
     allDay: false,
   },
   {
     start: new Date(2020, 4, 10),
-    end: new Date(2020, 5, 7),
+    end: new Date(2020, 5, 6),
     title: "Period 3",
     allDay: false,
   },
   {
     start: new Date(2020, 5, 7),
-    end: new Date(2020, 6, 5),
+    end: new Date(2020, 6, 4),
     title: "Period 4",
     allDay: false,
   },
   {
     start: new Date(2020, 6, 5),
-    end: new Date(2020, 7, 2),
+    end: new Date(2020, 7, 1),
     title: "Period 5",
     allDay: false,
   },
   {
     start: new Date(2020, 7, 2),
-    end: new Date(2020, 7, 30),
+    end: new Date(2020, 7, 29),
     title: "Period 6",
+    allDay: false,
+  },
+  {
+    start: new Date(2020, 7, 30),
+    end: new Date(2020, 8, 26),
+    title: "Period 7",
     allDay: false,
   },
 ];
