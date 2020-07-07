9f47a462e64f511055481491494e2ad7aa2bbb4a
jaybenaim <benaimjacob@gmail.com>
2020-07-07 13:12:03 -0400

    Updates

--- precache-manifest.87d1fddf0edd75eb5eea1db21f6302f4.js
+++ precache-manifest.87d1fddf0edd75eb5eea1db21f6302f4.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4a0c20351661a1e914d92a9b717434d9",
+    "revision": "1d1932c243646fc3e7c9ed0790dd0a68",
     "url": "/workTaxReviews/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/workTaxReviews/static/css/2.c7ed56f2.chunk.css"
   },
   {
-    "revision": "8ff3ba075ed088565f62",
-    "url": "/workTaxReviews/static/css/main.5723f7c3.chunk.css"
+    "revision": "5781471174be9b5f1d98",
+    "url": "/workTaxReviews/static/css/main.225cefa5.chunk.css"
   },
   {
     "revision": "a6ed8840f3e144a5e1a7",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/workTaxReviews/static/js/2.fa29065b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8ff3ba075ed088565f62",
-    "url": "/workTaxReviews/static/js/main.8082875b.chunk.js"
+    "revision": "5781471174be9b5f1d98",
+    "url": "/workTaxReviews/static/js/main.4ba4b78d.chunk.js"
   },
   {
     "revision": "a9b5967e48fd0111d783",
