618d8fcbf286d5763d4d49bf194e34da4e09b666
jaybenaim <benaimjacob@gmail.com>
2020-07-07 13:14:53 -0400

    Updates

---
+++ precache-manifest.edf3983883e5e03716c94e3cd366f5bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c4c2598ce615ffa157afc59c535fdb4",
+    "url": "/workTaxReviews/index.html"
+  },
+  {
+    "revision": "93da1c5dac265275dc75",
+    "url": "/workTaxReviews/static/css/2.c7ed56f2.chunk.css"
+  },
+  {
+    "revision": "28b87ea7495cc5a4b74e",
+    "url": "/workTaxReviews/static/css/main.86530f2c.chunk.css"
+  },
+  {
+    "revision": "93da1c5dac265275dc75",
+    "url": "/workTaxReviews/static/js/2.a288b157.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/workTaxReviews/static/js/2.a288b157.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28b87ea7495cc5a4b74e",
+    "url": "/workTaxReviews/static/js/main.2b97d6e0.chunk.js"
+  },
+  {
+    "revision": "a9b5967e48fd0111d783",
+    "url": "/workTaxReviews/static/js/runtime-main.d83f61cb.js"
+  }
+]);
